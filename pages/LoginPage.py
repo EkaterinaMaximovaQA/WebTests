a9781e1ff4acaecc8adb61a05caa17ab1a3bbbec
katemaximova <ekaterinamaximovaqa@gmail.com>
2026-01-15 17:22:24 +0300

    com

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,9 +1,12 @@
 import allure
 from pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
+import time
+
+#Локаторы = Адреса домов
 
 class LoginPageLocators:
-    LOGIN_FIELD = (By.ID, 'field_email')
+    LOGIN_FIELD = (By.XPATH, '//input[@id="field_email" and @name="st.email"]')
     PASSWORD_FIELD = (By.XPATH, '//input[@type="password" and @placeholder="Пароль"]')
     LOGIN_BUTTON = (By.XPATH, '//button[@type="submit" and contains(@class, "vkuiButton__modePrimary")]')
     QR_CODE_BUTTON = (By.XPATH,'//button[@label="Войти по QR-коду"]')
@@ -13,11 +16,19 @@ class LoginPageLocators:
     LOGIN_MAIL_BUTTON = (By.XPATH,'//*[@class="i ic social-icon __s __mailru"]')
     LOGIN_YANDEX_BUTTON = (By.XPATH,'//*[@class="i ic social-icon __s __yandex"]')
     ERROR_TEXT = (By.XPATH, '//span[contains(@class, "LoginForm-module__error")]')
+    RECOVER_BUTTON = (By.XPATH, '//a[.//span[text()="Восстановить"]]')
+    CANCELLATION_BUTTON = (By.XPATH, '//span[@class="vkuButton__content" and text()="Отмена"]')
+
+
+
+
+#Хелперы = Почтальон, который ходит по этим адресам
 
 class LoginPageHelper(BasePage):
     def __init__(self,driver):
         self.driver = driver
         self.check_page()
+
 
     def check_page(self):
         self.find_element(LoginPageLocators.LOGIN_FIELD)
@@ -30,6 +41,9 @@ class LoginPageHelper(BasePage):
         self.find_element(LoginPageLocators.LOGIN_MAIL_BUTTON)
         self.find_element(LoginPageLocators.LOGIN_YANDEX_BUTTON)
 
+
+
+
     @allure.step('Нажимаем на кнопку "Войти"')
     def click_login(self):
         self.attach_screen_shot()
@@ -40,11 +54,24 @@ class LoginPageHelper(BasePage):
         self.attach_screen_shot()
         return self.find_element(LoginPageLocators.ERROR_TEXT).text
 
-    @allure.step('вводим логин')
-    def enter_login(self,login_text):
-        login_field = self.find_element(LoginPageLocators.LOGIN_FIELD)
-        login_field.clear()
-        login_field.send_keys(login_text) #(send_keys)метод Selenium, который:Имитирует фокус на элементе (как если бы мы кликнули в поле),Имитирует нажатие клавиш на клавиатуре для каждого символа,Последовательно "печатает" переданный текст
+
+    @allure.step('Заполняем поле логин')
+    def type_login(self,login):
+        self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys(login)
+        self.attach_screen_shot()
+
+    @allure.step('Заполняем поле пароля')
+    def type_password(self, password):
+        self.find_element(LoginPageLocators.LOGIN_FIELD).send_keys(password)
+        self.attach_screen_shot()
+
+    @allure.step('Переходим к восстановлению')
+    def click_recovery (self):
+        self.attach_screen_shot()
+        time.sleep(10)
+        self.find_element(LoginPageLocators.RECOVER_BUTTON).click()
+
+
 
 
 
