44c4a06a1bf5071c87fcead3dcd3eff7668b5a44
katemaximova <ekaterinamaximovaqa@gmail.com>
2026-01-20 13:16:56 +0300

    testRegistration

--- pages/BasePage.py
+++ pages/BasePage.py
@@ -15,10 +15,16 @@ class BasePage:
             return WebDriverWait(self.driver,time).until(expected_conditions.element_to_be_clickable(locator),
                                                          message=f'не удалось найти локатор {locator}')
 
+    def find_elements(self, locator, time=5):
+        return WebDriverWait(self.driver, time).until(
+            expected_conditions.visibility_of_all_elements_located(locator),
+            message=f"Не удалось найти элементы {locator}"
+        )
 
     @allure.step('Открываем страницу')
     def get_url(self,url):
         return self.driver.get(url)
+
 
     def attach_screen_shot(self):
         allure.attach(self.driver.get_screenshot_as_png(),'скриншот',allure.attachment_type.PNG)
